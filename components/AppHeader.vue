db161e7b11228ea137c6579ec0603311fb350898
Laura <laura@blueplanet.com.tw>
2023-08-15 17:00:40 +0800

    core: add nuxt2

---
+++ components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header-container">
+    <h1>Nuxt2 Demo</h1>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'AppHeader',
+}
+</script>
+<style>
+.header-container {
+  background-color: #f2f6fc;
+  display: flex;
+  justify-content: center;
+}
+h1 {
+  color: #c0c4cc;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <AppHeader></AppHeader>
+    <Nuxt />
+  </div>
+</template>
+<script setup lang="ts">
+import AppHeader from '../components/AppHeader.vue'
+</script>
+<style>
+* {
+  font-family: monospace;
+}
+</style>
